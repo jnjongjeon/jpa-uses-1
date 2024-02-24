3e01ac1e7bf723075efc6286d28901c694b381b0
Allrace <jeonij23@gmail.com>
2024-02-24 17:58:49 +0900

    feat: 상품등록 및 상품 수정

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,18 +1,16 @@
 package jpabook.jpashop.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
-public class HelloController {
+public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        log.info();
+        log.info("home controller");
         return "home";
     }
 }
